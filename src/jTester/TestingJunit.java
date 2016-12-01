43217156b1eb0e6fbfd1b4ac8a360b3ab3d9e9a4
Blake <Blake@72.166.238.96>
2016-12-01 11:52:10 -0700

    Junit code to test

---
+++ src/jTester/TestingJunit.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jTester;
+
+/**
+ *
+ * @author Blake
+ */
+public class TestingJunit {
+    public int adder(String x, String y){
+        int num1 = Integer.parseInt(x);
+        int num2 = Integer.parseInt(y);
+        return num1 + num2;
+    }
+}
